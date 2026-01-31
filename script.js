4dc10ddf0fd186af5787046d3f526b7069a0bb30
yuktheman <chaudharykamala001@gmail.com>
2026-01-31 13:45:47 +0530

    Refactor here function for improved readability

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
+function here() {
+  const phonenumber = "9034538737";
 
-
-
-function here(){
-  let phonrnumber = '9034538737'
-let msg = ' Yukeshe, ekdam thulo breaking news chha.
+  const msg = `Yukeshe, ekdam thulo breaking news chha.
 Dherai research, overthinking ra 27 ota mental breakdown pachi… ma officially gay ho bhanne confirm bhayo.
 Ma tyahi purano pagal saathi ho, bas setting matra thap upgrade bhayo.
-Aasha chha yo le kehi change gardaina, bas mero manche herne choice officially verified bhayo 😂'
-let encodedmsg = encodeURIComponent(msg)
-let url  = `https://wa.me/${phonrnumber}?text=${encodedmsg}`
-window.location.href=url
+Aasha chha yo le kehi change gardaina 😂`;
 
+  const encodedmsg = encodeURIComponent(msg);
+  const url = `https://wa.me/${phonenumber}?text=${encodedmsg}`;
+
+  window.location.href = url;
 }
