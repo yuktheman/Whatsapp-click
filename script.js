430c330a6992aff7bdc7fc17fa3b17024f480bba
yuktheman <chaudharykamala001@gmail.com>
2026-01-31 13:35:57 +0530

    Update message in here function

--- script.js
+++ script.js
@@ -3,8 +3,12 @@
 
 function here(){
   let phonrnumber = '9034538737'
-let msg = 'Ma auta khatey ho  mero naam saroj motey ho ani ma dheroi motey xu ani ma dheroi hagxu ,, hagera hagera toilet bharinxa hapta hapta ma'
+let msg = ' Yukeshe, ekdam thulo breaking news chha.
+Dherai research, overthinking ra 27 ota mental breakdown pachi… ma officially gay ho bhanne confirm bhayo.
+Ma tyahi purano pagal saathi ho, bas setting matra thap upgrade bhayo.
+Aasha chha yo le kehi change gardaina, bas mero manche herne choice officially verified bhayo 😂'
 let encodedmsg = encodeURIComponent(msg)
 let url  = `https://wa.me/${phonrnumber}?text=${encodedmsg}`
 window.location.href=url
+
 }
